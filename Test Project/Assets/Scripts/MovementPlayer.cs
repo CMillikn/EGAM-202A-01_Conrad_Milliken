816a8769a52dc8aacd340ea3b49aa8be888de3e8
Mason <conrad.milliken@gmail.com>
2024-09-09 21:39:51 -0700

    yeah

---
+++ Test Project/Assets/Scripts/MovementPlayer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MovementPlayer : MonoBehaviour
+{
+    //my cool variable
+    bool ConradMilliken = true;
+
+    
+    
+
+
+    void Start()
+    {
+        //my awesome typo
+        if ( ConradMilliken == true)
+        {
+            Debug.Log("do literally nothing lol");
+        }
+    }
+
+    
+    void Update()
+    {
+        //my intellisense demonstration, just for you
+        if (ConradMilliken == true)
+        {
+            Debug.Log("unleash the 6 billion terabyte zip bomb;");
+        }
+    }
+}
